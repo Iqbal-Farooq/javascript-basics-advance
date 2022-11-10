fcfe790c375c4280a54fc213dc02164fa8b25445
iqbal-Farooq <akashfarooq222@gmail.com>
2022-11-10 12:03:51 +0530

    new Example

--- currying.js
+++ currying.js
@@ -1,5 +1,3 @@
-
-// currying iin js
 function sum(a){
     return function(b){
         return function(c){
@@ -12,9 +10,28 @@ var res=sum(9)(2)(3);
 console.log(res);
 
 
-// 
+// multiply
 function mul(x,y){
     console.log(x*y);
 }
 var res=mul.bind(this,2)
 res(3);
+
+// new example;
+var adds=function(a){
+    return function(b){
+        return function(c){
+            return a+b+c
+        }
+        
+          
+        
+    }
+}
+var Add1=adds(8);
+var Add3=Add1(4);
+var Add4=Add3(5);
+console.log(Add4);
+
+// or we can directly print like this
+console.log(adds(2)(7)(9));
