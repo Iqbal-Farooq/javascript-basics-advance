e7e8233d1c0a2a845289cd62a991efb942daa991
IQBAL AHMAD <116335338+Iqbal-Farooq@users.noreply.github.com>
2022-12-12 14:35:44 +0530

    Add files via upload
    LETS start REACT

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+
+function ExpenseItem(){
+   
+    return (
+        <div className="ex">
+           <h3>EXPENSE ITEMS </h3>
+           <h3>Food Rs 10</h3>
+           <h3>Petrol Rs 100</h3>
+           <h3> Movies Rs 200</h3>
+        </div>
+
+    )
+    
+    
+}
+export default ExpenseItem;
